ac14d0f533c2fea741c89d52f275ea4708f301ee
abashaaji <134337508+abashaaji@users.noreply.github.com>
2025-05-05 09:25:58 -0700

    Add files via upload

---
+++ Project1_expressions_alert/main.js
@@ -0,0 +1,17 @@
+// Alert method
+function showAlert() {
+    alert("Hello this is an alert ");
+}
+// docment.write method
+var playerName = "Abas Haji";
+document.write(playerName);
+
+let message = "He said , \"i can\'t believe it\""; 
+// string that contains an apostrophe and/or quotation mark
+document.write("Don/'t come my space unless you will be kicked out");
+
+var B = "concatenated" + " string" 
+document.write(B);
+
+let family = "Rer Haji", sister = "fartun", brother = "Ali",  mother = "Binti";
+document.write(mother)
