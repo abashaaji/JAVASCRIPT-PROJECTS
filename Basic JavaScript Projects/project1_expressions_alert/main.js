362e239f1d11a7d52744f02ad9354d1ca758f708
abashaaji <134337508+abashaaji@users.noreply.github.com>
2025-05-05 13:04:38 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/project1_expressions_alert/main.js
@@ -0,0 +1,27 @@
+// Alert method
+function showAlert() {
+    alert("Hello this is an alert ");
+}
+
+// creating two vars
+
+let Sent1 = "it is Summer time";
+let Sent2 = "stay away from bad people";
+
+//  Concatenate Sent1 and Sent2 to form a complete sentence.
+
+let sentenceTogether = Sent1 + Sent2;
+console.log(sentenceTogether)
+// docment.write method
+var playerName = "Abas Haji";
+document.write(playerName);
+
+let message = "He said , \"i can\'t believe it\""; 
+// string that contains an apostrophe and/or quotation mark
+document.write("Don/'t come my space unless you will be kicked out");
+
+var B = "concatenated" + " string" 
+document.write(B);
+// multiple str
+let family = "Rer Haji", sister = "fartun", brother = "Ali",  mother = "Binti";
+document.write(mother)
