56a2edd32d006b2e6586178dc9f525f1d4828891
abashaaji <134337508+abashaaji@users.noreply.github.com>
2025-05-07 10:06:23 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/project1_expressions_alert/Project4_dictionaries/main.js
@@ -0,0 +1,10 @@
+function showFamily() {
+    let family = {
+      father: "Omar",
+      mother: "Binti",
+      sister: "Ali"
+    };
+  
+    document.getElementById("Dictionary").innerHTML = family.mother;
+  }
+  
